3e78e730985d52cdedcb0311cb9a433288557923
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-04-24 20:48:35 -0300

    bugs2

--- src/schemas/transacaoSchema.js
+++ src/schemas/transacaoSchema.js
@@ -1,6 +1,6 @@
 import joi from "joi";
 
-export const transacaoSchema = Joi.object({
+export const transacaoSchema = joi.object({
     tipo: joi.string().required(),
     valor: joi.number().precision(2).required(),
     descricao:joi.string().required()
