e1fd0f58265adeab9eb53db5027430a1786ca06d
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-04-24 20:43:57 -0300

    bugs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import router from "./routes/indexRouter";
+import router from "./routes/indexRouter.js";
 
 const app = express(); //app do servidor
 app.use(cors());
