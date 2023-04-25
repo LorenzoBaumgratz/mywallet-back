c8997557aa089aa0f1bf9eea8925c84b5596b59c
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-04-25 00:35:44 -0300

    pls just work

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-import { db } from "../database/database"
+import { db } from "../database/database.js"
 
 export async function authValidation(req,res,next){
     const { authorization } = req.headers
